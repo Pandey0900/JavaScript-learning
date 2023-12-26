89c7a360a89147ee9a5eff4d2d76681dadaad952
Saurabh Pandey <144327750+Pandey0900@users.noreply.github.com>
2023-12-26 10:29:48 +0000

    DataType

---
+++ 01_basics/02_DataType.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Saurabh")
+
+
+let name = "Pandey"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => (this data type use when we need higher number like zerodha in share market data.)
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => (output of  let state)
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
