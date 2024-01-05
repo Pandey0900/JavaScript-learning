fd486484862b2bb8f0979a0b349544e693889c33
Saurabh Pandey <144327750+Pandey0900@users.noreply.github.com>
2024-01-05 07:24:55 +0000

    string methods

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId); //false
+//console.log(id === anotherId); //false
 
  const bigNumber = 3456543576654356754n
 
@@ -29,9 +29,37 @@ let myObj = {
 }
 
 const myFunction = function(){
-    console.log("Hello world");
+ //   console.log("Hello world");
 }
 
-console.log(typeof bigNumber);
+//console.log(typeof bigNumber);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ 
+// stack (primitive) , Heap (Non primitive)
+ let myName = "Saurabh Pandey";
+
+ let anotherName =myName
+     anotherName ="Roshan Pandey"
+
+  console.log(myName)
+  console.log(anotherName)
+
+  let userOne = {
+    email : "saurabh@gmail.com",
+    upi : "saurabh@sbi"
+  }
+ 
+  let userTwo = userOne
+
+  console.log(userOne.email)  //saurabh@gmail.com
+  console.log(userTwo.email)  //saurabh@gmail.com
+
+
+userTwo.email ="rosan@gmail.com"
+ console.log(userOne.email) //rosan@gmail.com
+ console.log(userTwo.email) //rosan@gmail.com
+
+
